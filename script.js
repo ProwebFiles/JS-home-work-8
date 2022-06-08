173e23f6f41c118719202f43af93f5ee0ebe8651
ProwebFiles <95128372+ProwebFiles@users.noreply.github.com>
2022-06-08 12:07:04 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+// Задание 1
+
+// const data = ['Иван', 'Иванов', 'Отдел разработки', 'Программист', 2000];
+// function func(emp){
+//     let [name, surname, department, position, salary] = emp;
+//     console.log(name, surname, department, position, salary);
+// }
+// func(data);
+
+// Задание 2
+
+// const data = ['Иван', 'Иванов', 'Отдел разработки', 'Программист', 2000];
+// function func(emp){
+//     let [name, surname, info] = emp;
+//     console.log(name, surname, info);
+// }
+// func(data);
+
+// Задание 3
+
+// const data = ['Иван', 'Иванов', 'Отдел разработки'];
+// function func(emp){
+//     let [name, surname, department , position = 'Junior'] = emp;
+//     console.log(name, surname, department , position);
+// }
+// func(data);
+
+// Задание 4
+
+// const data = {
+//     color: 'red',
+//     width: 400,
+//     height: 500
+// }
+// function func(options){
+//     let {color, width, height} = options;
+//     console.log(color, width, height);
+// }
+// func(data);
